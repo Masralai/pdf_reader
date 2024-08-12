28f90b95b604ef26dfed0827eb09bc46d8f0253e
Masralai <154480206+Masralai@users.noreply.github.com>
2024-08-12 19:03:11 +0530

    pdf can be read now

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import PyPDF2
 
-PyPDF2.PdfReader('c.pdf')
+a= PyPDF2.PdfReader('c.pdf')
 
+str=""
+for i in range(1,11):
+    str+=a.pages[i].extract_text()
 
+print(str)    
---
+++ test.py
@@ -0,0 +1,14 @@
+import PyPDF2
+
+a= PyPDF2.PdfReader('c.pdf')
+
+
+#print(a.is_encrypted)
+#print(len(a.pages))
+
+print(a.metadata)
+print(a.page_layout)
+print(a.pages[2].extract_text())
+
+for i in range(10):
+    print(a.pages[i].extract_text())
